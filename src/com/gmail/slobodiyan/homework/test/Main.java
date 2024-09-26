d644cd2fdd4a77674fd84ccf3d02bded960e675e
Dmytro Slobodian <Slobodiyan@gmail.com>
2024-09-26 23:10:49 +0300

    Add app description

--- src/com/gmail/slobodiyan/homework/test/Main.java
+++ src/com/gmail/slobodiyan/homework/test/Main.java
@@ -2,6 +2,7 @@ package com.gmail.slobodiyan.homework.test;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("My Git app for Users");
+        System.out.println("My Git app for Users.");
+        System.out.println("The informative application.");
     }
 }
