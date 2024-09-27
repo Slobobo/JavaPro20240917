9b67a791410abcf8f6d239712214808155db0ced
Dmytro Slobodian <Slobodiyan@gmail.com>
2024-09-28 00:47:45 +0300

    New branch

--- src/com/gmail/slobodiyan/homework/test/Main.java
+++ src/com/gmail/slobodiyan/homework/test/Main.java
@@ -4,5 +4,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("My Git app for Users.");
         System.out.println("The informative application.");
+        System.out.println("New branch");
+
     }
 }
